2d6dc62a3decd4a1d942954c1359e64b1c308029
Danielrxz <danielrodigome@gmail.com>
2026-08-02 19:01:55 -0700

    Actualizar tiktok.js

--- commands/dow/tiktok.js
+++ commands/dow/tiktok.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 const NYX_BASE = 'https://nyxdlapi.vercel.app'
 const NYX_TT_URL = `${NYX_BASE}/api/downloads/tiktok`
-const NYX_API_KEY = 'Danielrxz_miku67'
+const NYX_API_KEY = 'nyx_ReSxJ4ausLzmb6-HIXoZoOdmHUyCbvd0'
 
 export default {
   command: ['tiktok', 'tt'],
