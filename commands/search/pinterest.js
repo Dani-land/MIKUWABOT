2f786425b98ca805027f02adfc02b73cb1ad3b54
Danielrxz <danielrodigome@gmail.com>
2026-08-02 23:41:28 -0700

    Actualizar pinterest.js

--- commands/search/pinterest.js
+++ commands/search/pinterest.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 const NYX_API_URL = 'https://nyxdlapi.vercel.app/api/search/pinterest'
-const NYX_API_KEY = 'nyx_ReSxJ4ausLzmb6-HIXoZoOdmHUyCbvd0'
+const NYX_API_KEY = 'nyx_vDSYgjTlKOOLhz-_XmojwHjvH1_hp5c2'
 
 async function searchPinterest(query, limit) {
   const url = `${NYX_API_URL}?q=${encodeURIComponent(query)}&limit=${limit}&apikey=${NYX_API_KEY}`
