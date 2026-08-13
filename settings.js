3e9fb9ca7ce2b097e2a11857b5f16ffc130ec7f8
Danielrxz <danielrodigome@gmail.com>
2026-08-12 23:43:14 -0700

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,10 @@ global.pairing_code = true
 global.number_bot = ''
 
 global.api = {
-  url: 'https://nyxdlapi.vercel.app'
+  url: 'https://api.stellarwa.xyz',
+  key: 'Daniel_rxz'
 }
+
 
 global.bot = {
   api: 'https://nyxdlapi.vercel.app',
