f116cad412692eadfb5cd7e0ec8ed3e0444bdfd5
Danielrxz <danielrodigome@gmail.com>
2026-09-07 19:15:29 -0700

    Actualizar pinterest.js

--- commands/search/pinterest.js
+++ commands/search/pinterest.js
@@ -4,7 +4,7 @@ const NYX_API_URL = 'https://nyxdlapi.vercel.app/api/search/pinterest'
 const NYX_API_KEY = 'nyx_vDSYgjTlKOOLhz-_XmojwHjvH1_hp5c2'
 
 async function searchPinterest(query, limit) {
-  const url = `\( {NYX_API_URL}?q= \){encodeURIComponent(query)}&limit=\( {limit}&apikey= \){NYX_API_KEY}`
+  const url = `${NYX_API_URL}?q=${encodeURIComponent(query)}&limit=${limit}&apikey=${NYX_API_KEY}`
   const res = await fetch(url)
   const text = await res.text()
 
