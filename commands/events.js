b1e8f641a822789f4b7178221b91769ff99ac584
Danielrxz <danielrodigome@gmail.com>
2026-09-15 00:38:07 -0700

    Actualizar events.js

--- commands/events.js
+++ commands/events.js
@@ -77,8 +77,8 @@ export const participantsUpdate = async (client, anu) => {
 
             // ==================== BIENVENIDA ====================
             if (anu.action === 'add' && chat?.welcome && isPrimary) {
-                const caption = `ꨄ︎ 𝔹𝕚𝕖𝕟𝕧𝕖𝕟𝕚𝕕𝕠\n\n` +
-                    `☁︎ ${pushName}\n` +
+                const caption = `ฅ^•ﻌ•^ฅ ᗷIᗴᑎᐯᗴᑎIᗪO(ᗩ)\n\n` +
+                    `☁︎ ${phone}\n` +
                     `ꕤ ᘜᖇᑌᑭO ›⠀⠀${metadata.subject}\n` +
                     `ʕ·ᴥ·ʔ ᗰIᗴᗰᗷᖇOՏ ›⠀${memberCount}\n\n` +
                     `𝚄𝚜𝚊 *#𝚑𝚎𝚕𝚙* 𝚙𝚊𝚛𝚊 𝚟𝚎𝚛 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚌𝚘𝚖𝚊𝚗𝚍𝚘𝚜.`
@@ -92,8 +92,8 @@ export const participantsUpdate = async (client, anu) => {
 
             // ==================== DESPEDIDA ====================
             if ((anu.action === 'remove' || anu.action === 'leave') && chat?.welcome && isPrimary) {
-                const caption = `᯽ 𝔾𝕠𝕠𝕕𝕓𝕪𝕖\n\n` +
-                    `ᰔᩚ ${pushName}\n` +
+                const caption = `᯽ Ȃ̈D̑̈Ȋ̈Ȏ̈S̑̈\n\n` +
+                    `ᰔᩚ ${phone}\n` +
                     `ʕ·ᴥ·ʔ ᗰIᗴᗰᗷᖇOՏ ›⠀${memberCount}\n\n` +
                     `✿ 𝙴𝚜𝚙𝚎𝚛𝚎𝚖𝚘𝚜 𝚚𝚞𝚎 𝚛𝚎𝚐𝚛𝚎𝚜𝚎𝚜 𝚙𝚛𝚘𝚗𝚝𝚘`
 
