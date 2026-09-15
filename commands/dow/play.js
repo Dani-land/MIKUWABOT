ac3259a20ba3c724801f89e245cc4fe672a89efa
Danielrxz <danielrodigome@gmail.com>
2026-09-15 16:13:43 -0700

    Actualizar play.js

--- commands/dow/play.js
+++ commands/dow/play.js
@@ -9,7 +9,7 @@ const DVYER_AUDIO = 'https://dv-yer-api.online/ytmp3'
 const DVYER_VIDEO = 'https://dv-yer-api.online/ytmp4'
 
 const NEWSLETTER_JID = '120363420575743790@newsletter'
-const NEWSLETTER_NAME = '✰ Hatsune Miku / Wa'
+const NEWSLETTER_NAME = 'ᰔᩚ Mιкυ Pʀσʝєᴄтѕ'
 
 const HEADERS = {
   'user-agent':
